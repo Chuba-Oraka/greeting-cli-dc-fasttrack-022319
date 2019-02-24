a24d85dcba72c216e54e05fcb3e563b73af2eae8
Chuba Oraka <chuba.oraka@student.potomac.edu>
2019-02-24 15:50:45 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,9 +2,9 @@
 
 def greeting(name)
 # <<<<<<< HEAD
-  # p "Hello #{name.strip}. It's nice to meet you."
+  p "Hello #{name.strip}. It's nice to meet you."
 # =======
-  p ""
-  p "Hello #{name}. It's nice to meet you."
+  # p ""
+  # p "Hello #{name}. It's nice to meet you."
 # >>>>>>> e018c1974f0cd621f0e2b392ed9d93af72f69eba
 end
